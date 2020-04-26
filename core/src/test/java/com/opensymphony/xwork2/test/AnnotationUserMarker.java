4a8315fa169b850b6097de498d950c2e91669231
Lukasz Lenart <lukaszlenart@apache.org>
2020-04-26 07:50:13 +0200

    WW-5017 Drops deprecated Validation annotation as not needed

--- core/src/test/java/com/opensymphony/xwork2/test/AnnotationUserMarker.java
+++ core/src/test/java/com/opensymphony/xwork2/test/AnnotationUserMarker.java
@@ -20,7 +20,6 @@ package com.opensymphony.xwork2.test;
 
 import com.opensymphony.xwork2.validator.annotations.ExpressionValidator;
 import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
-import com.opensymphony.xwork2.validator.annotations.Validation;
 import com.opensymphony.xwork2.validator.annotations.Validations;
 
 /**
@@ -29,16 +28,14 @@ import com.opensymphony.xwork2.validator.annotations.Validations;
  * @author Mark Woon
  * @author Rainer Hermanns
  */
-@Validation(
-        validations = @Validations(
-                requiredFields = {
-                    @RequiredFieldValidator(fieldName = "email", shortCircuit = true, message = "You must enter a value for email."),
-                    @RequiredFieldValidator(fieldName = "email2", shortCircuit = true, message = "You must enter a value for email2.")
-                },
-                expressions = {
-                        @ExpressionValidator(shortCircuit = true, expression = "email.equals(email2)", message = "Email not the same as email2" )
-                }
-        )
+@Validations(
+    requiredFields = {
+        @RequiredFieldValidator(fieldName = "email", shortCircuit = true, message = "You must enter a value for email."),
+        @RequiredFieldValidator(fieldName = "email2", shortCircuit = true, message = "You must enter a value for email2.")
+    },
+    expressions = {
+        @ExpressionValidator(shortCircuit = true, expression = "email.equals(email2)", message = "Email not the same as email2")
+    }
 )
 public interface AnnotationUserMarker {
 }
